ec088551133369bfb5ab0482e8ca4d3f3f147e77
Danilo Batson <djbatson19@gmail.com>
2022-02-18 01:17:03 -0800

    feat: dependencies install

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -51,7 +54,7 @@ export default function Home() {
           </a>
         </div>
 
-        
+
       </main>
 
       <footer className={styles.footer}>
